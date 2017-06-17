7dd6b0c318521e590cf0fce839cc01cc99191d2d
Christian Klaussner <developer@klaussner.me>
2017-06-17 12:47:33 +0200

    Update Svelte to 1.22.4

--- compiler/package.js
+++ compiler/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'svelte:compiler',
-  version: '1.3.1',
+  version: '1.4.0',
   summary: 'Svelte compiler',
   git: 'https://github.com/meteor-svelte/meteor-svelte.git',
   documentation: '../README.md'
@@ -8,7 +8,7 @@ Package.describe({
 
 Package.registerBuildPlugin({
   name: 'svelte-compiler',
-  use: ['ecmascript@0.6.1', 'svelte:core@1.2.1'],
+  use: ['ecmascript@0.6.1', 'svelte:core@1.3.0'],
   sources: [
     'plugin.js'
   ]
--- core/package.js
+++ core/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'svelte:core',
-  version: '1.2.1',
+  version: '1.3.0',
   summary: 'Svelte compiler core',
   git: 'https://github.com/meteor-svelte/meteor-svelte.git'
 });
@@ -8,7 +8,7 @@ Package.describe({
 Npm.depends({
   htmlparser2: '3.9.2',
   'source-map': '0.5.6',
-  svelte: '1.12.1'
+  svelte: '1.22.4'
 });
 
 Package.onUse(function (api) {
