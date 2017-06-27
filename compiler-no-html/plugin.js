f3c38c880d711364ef2b585fdfd1e5b7471f2cad
Christian Klaussner <developer@klaussner.me>
2017-06-27 20:10:15 +0200

    Update version number in deprecation message

--- compiler-no-html/plugin.js
+++ compiler-no-html/plugin.js
@@ -1,7 +1,7 @@
 console.warn(
   '\x1b[33m%s\x1b[0m',
   'Warning: `svelte:compiler-no-html` is deprecated. ' +
-  'Please use `svelte:compiler` 1.4.0 or newer and configure it to ignore HTML files.'
+  'Please use `svelte:compiler` 1.23.4_1 or newer and configure it to ignore HTML files.'
 );
 
 Plugin.registerCompiler({
