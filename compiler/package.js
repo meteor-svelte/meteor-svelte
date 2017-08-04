2257ad724366b5bc585e2feb4bd4a685d2ee147e
Christian Klaussner <developer@klaussner.me>
2017-08-04 22:13:49 +0200

    Enable runtime checks during development

--- compiler/package.js
+++ compiler/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'svelte:compiler',
-  version: '1.27.0_1',
+  version: '1.27.0_2',
   summary: 'Svelte compiler',
   git: 'https://github.com/meteor-svelte/meteor-svelte.git',
   documentation: '../README.md'
@@ -8,7 +8,7 @@ Package.describe({
 
 Package.registerBuildPlugin({
   name: 'svelte-compiler',
-  use: ['ecmascript@0.6.1', 'svelte:core@1.27.0_1'],
+  use: ['ecmascript@0.6.1', 'svelte:core@1.27.0_2'],
   sources: [
     'plugin.js'
   ]
