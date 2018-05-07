29cc47d3b19dbcf9142246f0a8c7453632748256
Christian Klaussner <christian@klaussner.me>
2018-05-07 10:02:20 +0200

    Remove deprecated `compiler-no-html` package

--- compiler-no-html/README.md
+++
@@ -1,6 +0,0 @@
-# Svelte compiler (ignores HTML files)
-
-Use [the magical disappearing UI framework](https://svelte.technology) with Meteor.
-
-**Warning:** This package is deprecated.
-Please use `svelte:compiler` 1.23.4_1 or newer and configure it to ignore HTML files.
--- compiler-no-html/plugin.js
+++
@@ -1,9 +0,0 @@
-console.warn(
-  '\x1b[33m%s\x1b[0m',
-  'Warning: `svelte:compiler-no-html` is deprecated. ' +
-  'Please use `svelte:compiler` 1.23.4_1 or newer and configure it to ignore HTML files.'
-);
-
-Plugin.registerCompiler({
-  extensions: ['svelte'],
-}, () => new SvelteCompiler);
