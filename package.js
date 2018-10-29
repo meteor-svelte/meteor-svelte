9346674acc312daf599e16e9edbfd40709319f98
Christian Klaussner <christian@klaussner.me>
2018-10-29 18:24:12 +0100

    Update Svelte to 2.15.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'svelte:compiler',
-  version: '2.13.5_2',
+  version: '2.15.0_1',
   summary: 'Svelte compiler',
   git: 'https://github.com/meteor-svelte/meteor-svelte.git',
   documentation: 'README.md'
@@ -22,7 +22,7 @@ Package.registerBuildPlugin({
     'find-up': '2.1.0',
     htmlparser2: '3.9.2',
     'source-map': '0.5.6',
-    svelte: '2.13.5'
+    svelte: '2.15.0'
   }
 });
 
