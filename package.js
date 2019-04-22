5284c2a19f9e033ec16e756f3d66ae0841c00896
Christian Klaussner <christian@klaussner.me>
2019-04-22 18:19:28 +0200

    Update Svelte to 3.0.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'svelte:compiler',
-  version: '2.16.1_1',
+  version: '3.0.0_1',
   summary: 'Svelte compiler',
   git: 'https://github.com/meteor-svelte/meteor-svelte.git',
   documentation: 'README.md'
@@ -9,25 +9,25 @@ Package.describe({
 Package.registerBuildPlugin({
   name: 'svelte-compiler',
   use: [
-    'babel-compiler@6.24.7||7.0.0',
-    'caching-compiler@1.1.11',
-    'ecmascript@0.9.0'
+    'babel-compiler@7.3.4',
+    'caching-compiler@1.2.1',
+    'ecmascript@0.12.7'
   ],
   sources: [
     'SvelteCompiler.js',
     'plugin.js'
   ],
   npmDependencies: {
-    '@babel/runtime': '7.1.2',
-    'find-up': '2.1.0',
-    htmlparser2: '3.9.2',
+    '@babel/runtime': '7.4.3',
+    'find-up': '3.0.0',
+    htmlparser2: '3.10.1',
     'source-map': '0.5.6',
-    svelte: '2.16.1'
+    svelte: '3.0.0'
   }
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.6');
+  api.versionsFrom('1.8');
   api.use('isobuild:compiler-plugin@1.0.0');
 
   // Dependencies for compiled Svelte components (taken from `ecmascript`).
